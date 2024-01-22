946ee5c702cba5a85d1351a0482828742f2d9730
zokirov1234 <zokirov123404321@gmail.com>
2024-01-22 22:32:29 +0500

    first commit

---
+++ src/main/java/com/example/demox/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demox.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> sayHello() {
+
+        return ResponseEntity.ok("hello");
+    }
+}
