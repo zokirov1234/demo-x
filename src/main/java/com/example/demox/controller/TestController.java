385780f880668e09719948472e7ba40cb0590bdb
zokirov1234 <zokirov123404321@gmail.com>
2024-01-23 12:27:25 +0500

    fixed some bugs

--- src/main/java/com/example/demox/controller/TestController.java
+++ src/main/java/com/example/demox/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demox.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
     @GetMapping("/hello")
     public ResponseEntity<?> sayHello() {
-
+        log.info("say hello");
         return ResponseEntity.ok("hello");
     }
 }
